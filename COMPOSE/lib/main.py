906f3016ff7619b2fbff89246ef411f8effface3
alkina <txtnnn21@gmail.com>
2023-03-31 11:38:55 +0300

    практическая 3

--- COMPOSE/lib/main.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-MONGO_URL = "mongodb://ovoshebaza:27017"
-client = MongoClient(MONGO_URL)
-db = client.admin
-dbs_list = db.command("listDatabases")
-pprint(dbs_list)
--- Pr1/Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:alpine
-
-WORKDIR /app
-
-COPY . .
-
-CMD ["python", "main.py"]
